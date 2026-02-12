2a8f55c7d977ecd2979a1669f56942aa56cbe09c
RonnieRoy21 <ronnieroymwangi@gmail.com>
2026-02-12 03:40:30 +0300

    fifth commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,97 +14,70 @@ class Main extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('MealMind App'),
+        backgroundColor: Colors.green,
+      ),
       body: Container(
         height: screenSize.height,
         width: screenSize.width,
         color: Colors.grey,
         child: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'MealMind App',
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height:
+                    screenSize.height * 0.6, // set a max height for carousel
+                child: CarouselView(
+                  itemExtent: screenSize.width * 0.5,
+                  children: [
+                    Image.asset('assets/login.jpeg', fit: BoxFit.cover),
+                    Image.asset('assets/create.jpeg', fit: BoxFit.cover),
+                    Image.asset('assets/home.jpeg', fit: BoxFit.cover),
+                    Image.asset(
+                      'assets/a well organized macro table.jpeg',
+                      fit: BoxFit.cover,
+                    ),
+                    Image.asset('assets/checkout.jpeg', fit: BoxFit.cover),
+                  ],
                 ),
-                const SizedBox(height: 20),
-                const Text('Create account'),
-                InkWell(
-                  onTap: () => Image.asset(
-                    'assets/create.jpeg',
-                    height: screenSize.height,
-                    width: screenSize.width,
-                  ),
-                  child: Image.asset(
-                    'assets/create.jpeg',
-                    height: screenSize.height * 0.2,
-                    width: screenSize.width * 0.4,
-                  ),
-                ),
-                const Text('The Home Page'),
-                InkWell(
-                  onTap: () => Image.asset(
-                    'assets/create.jpeg',
-                    height: screenSize.height,
-                    width: screenSize.width,
-                  ),
-                  child: Image.asset(
-                    'assets/home.jpeg',
-                    height: screenSize.height * 0.2,
-                    width: screenSize.width * 0.4,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                const Text('Meal analysis and recommendation app'),
-                InkWell(
-                  onTap: () => Image.asset(
-                    'assets/create.jpeg',
-                    height: screenSize.height,
-                    width: screenSize.width,
-                  ),
-                  child: Image.asset(
-                    'assets/a well organized macro table.jpeg',
-                    height: screenSize.height * 0.2,
-                    width: screenSize.width * 0.4,
-                  ),
-                ),
-                const SizedBox(height: 10),
-
-                ElevatedButton(
-                  onPressed: () async {
-                    //downloading the app
-                    final Uri url = Uri.parse(mealMindApkUrl);
-                    try {
-                      if (await canLaunchUrl(url)) {
-                        await launchUrl(url);
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Could not launch URL')),
-                        );
-                      }
-                    } catch (e) {
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  final Uri url = Uri.parse(mealMindApkUrl);
+                  try {
+                    if (await canLaunchUrl(url)) {
+                      await launchUrl(url);
+                    } else {
                       ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text('Error launching URL: $e')),
+                        const SnackBar(content: Text('Could not launch URL')),
                       );
                     }
-                  },
-                  child: const Text('Download the MealMind App'),
-                ),
-                const SizedBox(height: 20),
-                const Text(
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Error launching URL: $e')),
+                    );
+                  }
+                },
+                child: const Text('Download the MealMind App'),
+              ),
+              const SizedBox(height: 20),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
                   'Enable "Install from unknown sources" if prompted to install the app.',
                   style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.redAccent,
+                    fontSize: 25,
+                    color: Colors.black,
                     fontWeight: FontWeight.bold,
                   ),
+                  textAlign: TextAlign.center,
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
